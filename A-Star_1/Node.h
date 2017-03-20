96f46f623df3feabe85a3f6b22833a62eec2d374
T0b1asN <tobiasnienhaus69@gmail.com>
2017-03-20 18:52:24 +0100

    Updated Readme (and a file, that wouldn't commit)

--- A-Star_1/Node.h
+++ A-Star_1/Node.h
@@ -14,13 +14,18 @@ std::ostream& operator<<(std::ostream& os, sf::Color c);
 class Node
 {
 private:
+	//the costs of the Node
 	float g, h, f;
 
+	//Position of the node
 	sf::Vector2i pos;
+	//neighbours of the Node
 	std::vector<Node> neighbours;
 
+	//is the Node an obstacle
 	bool obstacle = false;
 
+	//the node's parent
 	sf::Vector2i parent;
 public:
 	Node();
@@ -29,6 +34,7 @@ public:
 
 	sf::Vector2i getPos() { return pos; }
 
+	//getter and setter for the costs
 	float getF() { return f; }
 	float getG() { return g; }
 	float getH() { return h; }
@@ -37,19 +43,25 @@ public:
 	void setG(float pG) { g = pG; }
 	void setH(float pH) { h = pH; }
 
-	bool Node::operator==(const Node& rhs) const;
-
+	//adds the neighbours on a map
 	void addNbs(Map2_0& map);
+
+	bool Node::operator==(const Node& rhs) const;
 
 	std::vector<Node> getNbs() { return neighbours; }
 
+	//set this node to another
 	void setNode(Node& other);
+	//same as setNode() but without the neighbours
 	void setNode2(Node& other);
 
+	//draw the Node on the current window
 	void Draw();
 
+	//Color of the Node
 	sf::Color debCol = sf::Color::White;
 
+	//getters and setters for the obstacle state and the parent
 	void setObstacle(bool state) { obstacle = state; }
 	bool isObstacle() { return obstacle; }
 
--- README.md
+++ README.md
@@ -4,5 +4,15 @@ Just a little Test of an Pathfindingalgorithm.
 Completely free to use, CC0, you can use it, which doesn't mean, you should. :)
 It doesn't even work right now and it will not be the fastest A*-implementation. 
 
-### Markdown-Test
-This is also my 3rd project on GitHub, so I'm fairly new to this stuff, like a markdown-Readme, so I test it a bit. :)
+## Instructions
+To use the application, you need to locate a folder res, located in a folder in the folder of the executable.
+There you can put the image, that needs solving.
+After you have done that, you can open the executable, where you type the name of the picture,
+with extension. After that, you state, wether diagonal movement is allowed or not.
+
+## Restrictions of the image
+First of all, the larger, the slower.
+100 by 100 works, but it needs up to about 15 seconds.
+The start should be marked in green, the end in red. Non-passable tiles should be black.
+It doesn't matter, if you have more than 1 start or more than one finish.
+The program chooses the first start and the last end it finds.
